396d4243efed1b69bd5d14c52097ef602439055f
wyf_zqx <wyf_zqx@163.com>
2016-05-12 19:32:30 +0800

    "Complete Regular Expression"

--- js_cookbook/jscookbook_2_reg.js
+++ js_cookbook/jscookbook_2_reg.js
@@ -71,6 +71,7 @@ function reg_2_4() {
     last = 0;
   var resString = "";
 
+  //use RegExp exec method
   while((matchArray = pattern.exec(searchString)) != null){
     last = matchArray.index;
     resString += searchString.substring(first, last);
@@ -78,7 +79,11 @@ function reg_2_4() {
     first = pattern.lastIndex;
   }
   // finish off string
-  resString += searchString.substring(first,searchString.length);
+  resString = searchString.substring(first,searchString.length);
+
+  //or use replace and '&$'
+  resString = searchString.replace(pattern, "<span class=highlight>$&</span>");
+
   return resString;
 }
 
@@ -91,15 +96,42 @@ function reg_2_5() {
   alert(replacement);     //"Now is the place, this is the place";
 }
 
+//2.6(☆☆☆) Swap Words in a String Using Capturing Parentheses
+function reg_2_6() {
+  var name = "Abe Lincoln";
+  var pattern = /^(\w+)\s(\w+)$/;
+  var newName = name.replace(pattern, "$2, $1");
+  //$$:Allows a literal dollar sign ($) in replacement
+  //$&:Inserts matched substring
+  //$` Inserts portion of string before match
+  //$’ Inserts portion of string after match
+  //$n Inserts nth captured parenthetical value when using RegExp
+}
 
+//2.7(☆) Using Regular Expressions to Trim Whitespace
+//See Also str_1_10: Trimming Whitespace from the Ends of a String
+function reg_2_7() {
+  function leftTrim(str) { return str.replace(/^\s+/, ""); }
+  function rightTrim(str) { return str.replace(/\s+$/, ""); }
+}
 
+//2.8(☆☆) Replace Html Tags with Named Entities
+function reg_2_8() {
+  var pieceOfHtml = "<p>This is a <span>paragraph</span></p>";
+  pieceOfHtml = pieceOfHtml.replace(/</g, "&lt;");
+  pieceOfHtml = pieceOfHtml.replace(/>/g, "&gt;");
+}
 
-
-
-
-
-
-
-function paddingbottom() {
-
+//2.9(☆☆) Searching for Special Characters
+function reg_2_9() {
+  var re = /\\d/;
+  var pattern = "\\d{4}";
+  var str = "I want 1111 to find 3334 certain 5343 things 8484";
+  var re2 = new RegExp(pattern, "g");
+  var str2 = str.replace(re2, "****");
+  alert(str2);  //"I want **** to find **** certain **** things ****"
+  var pattern1 = pattern.replace(re, "\\D");
+  var re3 = new RegExp(pattern1, "g");
+  var str3 = str.replace(re3, "****");
+  alert(str3);  //"****nt 1111******** 3334******** 5343********8484"
 }
