3a327dea5b404380af3252c6fedec03194e5c5b3
wyfzqx <wyf_zqx@163.com>
2016-05-12 22:51:54 +0800

    "Chapter Number:Introduction"

---
+++ js_cookbook/jscookbook_4_num.js
@@ -0,0 +1,52 @@
+/**
+ * 2016-05-12 22:15
+ * Chapter 4.Working with Numbers and Math
+ */
+
+//Math Object:Does not have a constructor.All of the
+//object's functionality, its properties and methods, are static
+function introduction() {
+  var x = 1.1;
+  //Math.cell(x): Returns the smallest integer equal to or greater than x
+  Math.cell(x);   //2
+  //Math.floor(x): Returns the largest integer equal to or less than x
+  Math.floor(x);  //1
+  //Math.random(): [0, 1)
+  Math.random();
+  //Math.round(x): Rounds number to closest integer
+  Math.round(x);  //1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function lastpadding() {
+
+}
